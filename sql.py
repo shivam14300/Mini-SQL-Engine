7e915afc9ecae5ebe50a088ef7257bf0aedce5d3
shivam14300 <shivam.bansal@students.iiit.ac.in>
2019-09-03 03:24:28 +0530

    queries started

--- sql.py
+++ sql.py
@@ -40,20 +40,53 @@ def tablesData(tablename):
     return table_data
 
 
+TABLE_INFO = tablesInfo('files/metadata.txt')
+TABLE_DATA = defaultdict()
+    
+
+def QuerySolver(query):
+    # validating the query
+    if query[0].lower() != 'select':
+        print("Wrong query!!!")
+        return -1
+    if query[1].lower() == 'distinct':
+        if query[3].lower() != 'from':
+            print(1)
+            print("Wrong query!!!")
+            return -1
+    else:
+        if query[2].lower() != 'from':
+            print(query[2])
+            print("Wrong query!!!")
+            return -1
+    if query[1] == '*':
+        table_name = query[3]
+        if not TABLE_INFO[table_name]:
+            print('Wrong table name')
+            return -1
+        printlabel = TABLE_INFO[table_name]
+        printtable = TABLE_DATA[table_name]
+        printdata(printlabel,printtable)
+ 
+def printdata(tablelabel,tabledata):
+    # for i in tablelabel:
+    print(tablelabel)
+    for i in tabledata:
+        print(tuple(i))
+
+
+
 if __name__ == "__main__":
-    TABLE_INFO = tablesInfo('files/metadata.txt')
-    # print(TABLE_INFO)
-    TABLE_DATA = defaultdict()
     for name in TABLE_INFO:
         # print(name)
         TABLE_DATA[name] = tablesData(name)                
         # print(TABLE_DATA[name])
     
     try:
-        query = []
-        for i in range(1,len(sys.argv)):
-            query.append(sys.argv[i])
-
-        # QuerySolver(query)
+        query = sys.argv[1]
+        query = query.split(';')[0]
+        query = query.split(' ')
+        # print(query)
+        QuerySolver(query)
     except:
-        pass
+        print("Wrong query")
